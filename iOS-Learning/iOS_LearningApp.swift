4ca37e62fcd3bd53a7abcb0b857a14fb15322baf
Sudharsan D <devarajsudharsan@gmail.com>
2023-09-13 11:51:31 +0530

    Unwanted files

--- iOS-Learning/iOS_LearningApp.swift
+++ iOS-Learning/iOS_LearningApp.swift
@@ -9,12 +9,10 @@ import SwiftUI
 
 @main
 struct iOS_LearningApp: App {
-    let persistenceController = PersistenceController.shared
 
     var body: some Scene {
         WindowGroup {
-            ContentView()
-                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+            doTryCatchThrowsView()
         }
     }
 }
